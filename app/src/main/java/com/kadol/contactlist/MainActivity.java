2fcd0f703b7a363d9bea9110f9714ce7e25efebe
shahednissan <shahednissan@gmail.com>
2017-03-31 14:55:06 +0600

    SQLite database working perfectly

--- app/src/main/java/com/kadol/contactlist/MainActivity.java
+++ app/src/main/java/com/kadol/contactlist/MainActivity.java
@@ -1,62 +1,82 @@
 package com.kadol.contactlist;
 
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper dataBaseHelper;
+
+    int numberOfTimesLoadButtonClicked=0;
+
+    ArrayList<Contact> contactList=new ArrayList<>();
+
+    ContactAdapter contactAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Contact> contactList=new ArrayList<>();
-
         dataBaseHelper=new DataBaseHelper(this);
 
-        boolean state = dataBaseHelper.insertContact("Aaabbb","12345");
+        dataBaseHelper.onDelete();
 
-        if(state==true)
-            Toast.makeText(getApplicationContext(),"Database updated",Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(getApplicationContext(),"Database is not updated",Toast.LENGTH_SHORT).show();
+        insertDatabase();
 
-        Cursor res=dataBaseHelper.getData(1);
-
-        contactList.add(new Contact(res.getString(res.getColumnIndex(DataBaseHelper.CONTACTS_COLUMN_NAME)),
-                res.getString(res.getColumnIndex(DataBaseHelper.CONTACTS_COLUMN_PHONE))));
-
-
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-        contactList.add(new Contact("Aabbb","12345"));
-
+        contactList=dataBaseHelper.GroupContact();
 
         ListView listView=(ListView) findViewById(R.id.list);
 
-        ContactAdapter contactAdapter=new ContactAdapter(this,contactList);
+        contactAdapter=new ContactAdapter(this,fillingUpTheList());
 
         listView.setAdapter(contactAdapter);
 
 
+
+    }
+
+    private void insertDatabase() {
+
+        dataBaseHelper.insertContact("Aaabbb1","12345");
+        dataBaseHelper.insertContact("Aaabbb2","12345");
+        dataBaseHelper.insertContact("Aaabbb3","12345");
+        dataBaseHelper.insertContact("Aaabbb4","12345");
+        dataBaseHelper.insertContact("Aaabbb5","12345");
+        dataBaseHelper.insertContact("Aaabbb6","12345");
+        dataBaseHelper.insertContact("Aaabbb7","12345");
+        dataBaseHelper.insertContact("Aaabbb8","12345");
+        dataBaseHelper.insertContact("Aaabbb9","12345");
+        dataBaseHelper.insertContact("Aaabbb10","12345");
+        dataBaseHelper.insertContact("Aaabbb11","12345");
+        dataBaseHelper.insertContact("Aaabbb12","12345");
+        dataBaseHelper.insertContact("Aaabbb13","12345");
+        dataBaseHelper.insertContact("Aaabbb14","12345");
+        dataBaseHelper.insertContact("Aaabbb15","12345");
+        dataBaseHelper.insertContact("Aaabbb16","12345");
+        dataBaseHelper.insertContact("Aaabbb17","12345");
+        dataBaseHelper.insertContact("Aaabbb18","12345");
+        dataBaseHelper.insertContact("Aaabbb19","12345");
+        dataBaseHelper.insertContact("Aaabbb20","12345");
+        dataBaseHelper.insertContact("Aaabbb21","12345");
+        dataBaseHelper.insertContact("Aaabbb22","12345");
+    }
+
+    public List<Contact> fillingUpTheList(){
+        int startPoint=numberOfTimesLoadButtonClicked*10;
+        int endPoint=startPoint+10;
+
+        if(endPoint>contactList.size()){
+            endPoint=contactList.size()-1;
+        }
+
+        List<Contact> subListOf10= contactList.subList(startPoint,endPoint);
+
+        return  subListOf10;
 
     }
 }
