0260a67bd9a40b1be914058ee84321087086bf5c
shahednissan <shahednissan@gmail.com>
2017-03-31 13:24:38 +0600

    DatabaseHelper class is added

--- app/src/main/java/com/kadol/contactlist/MainActivity.java
+++ app/src/main/java/com/kadol/contactlist/MainActivity.java
@@ -1,12 +1,16 @@
 package com.kadol.contactlist;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper dataBaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Contact> contactList=new ArrayList<>();
+
+        dataBaseHelper=new DataBaseHelper(this);
+
+        boolean state = dataBaseHelper.insertContact("Aaabbb","12345");
+
+        if(state==true)
+            Toast.makeText(getApplicationContext(),"Database updated",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Database is not updated",Toast.LENGTH_SHORT).show();
+
+        Cursor res=dataBaseHelper.getData(1);
+
+        contactList.add(new Contact(res.getString(res.getColumnIndex(DataBaseHelper.CONTACTS_COLUMN_NAME)),
+                res.getString(res.getColumnIndex(DataBaseHelper.CONTACTS_COLUMN_PHONE))));
+
 
         contactList.add(new Contact("Aabbb","12345"));
         contactList.add(new Contact("Aabbb","12345"));
